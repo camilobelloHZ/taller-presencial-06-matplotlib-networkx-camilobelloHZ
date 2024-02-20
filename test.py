4baebbf083bd7ea6769aa74c0e959f2a2814ba7e
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-02-20 08:30:14 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,34 @@
+"""GitHub Classroom autograding script."""
+
+import os
+
+import pandas as pd
+
+from country_collaboration import main
+
+main(20)
+
+#
+# Retorna error si la carpeta output/ no existe
+if not os.path.exists("countries.csv"):
+    raise FileNotFoundError("File 'countries.csv' not found")
+
+#
+# Lee el contenido del archivo output.txt
+dataframe = pd.read_csv("countries.csv")
+dataframe = dataframe.set_index("country")
+
+assert dataframe["United States of America"] == 579
+assert dataframe["China"] == 273
+assert dataframe["India"] == 174
+assert dataframe["United Kingdom"] == 173
+assert dataframe["Italy"] == 112
+
+#
+#
+if not os.path.exists("co_occurrences.csv"):
+    raise FileNotFoundError("File 'co_occurrences.csv' not found")
+
+
+if not os.path.exists("network.png"):
+    raise FileNotFoundError("File 'network.pmg' not found")
